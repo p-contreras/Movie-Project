4afddad2d3ea0dfae7b34ac230326d3a4ba09f2d
Patricio Contreras <Pato@patricios-mbp.home>
2020-07-26 19:47:39 -0700

    BETA 1!!!  (Progress 26/7)

--- voting.py
+++ voting.py
@@ -1,46 +1,45 @@
-import random, operator
+import random, operator, time
 from datetime import date
 
 
-def FINAL(movie_info):
-	return "rifus is cheese"
-
-
-def PAIRINGS(movie_info):
-	# we'll use ASM pairing method (1 v 4, 2 v 3)
-	movies = movie_info[1]
-	# 1 v 4
-	pairing1 = [movies[0], movies[3]]
-	# 2 v 3
-	pairing2 = [movies[1], movies[2]]
-	print("The two films in this pairing are:", pairing1)
-	vote1 = int(input("How many people vote for: " + pairing1[0] + "? "))
-	vote2 = len(movie_info[0]["People"]) - vote1
-	if vote1 > vote2:
-		pairing1.pop()
-	elif vote2 > vote1:
-		pairing1.pop(0)
-	else:
-		print("Coin toss!")
-		result = random.choice(pairing1)
-		print(result + " passes")
-		pairing1 = [result]
-
-	print("The two films in this pairing are:", pairing2)
-	vote1 = int(input("How many people vote for: " + pairing2[0] + "? "))
-	vote2 = len(movie_info[0]["People"]) - vote1
-	if vote1 > vote2:
-		pairing2.pop()
-	elif vote2 > vote1:
-		pairing2.pop(0)
-	else:
-		print("Coin toss!")
-		result = random.choice(pairing2)
-		print(result + " passes")
-		pairing2 = [result]
-	# at this point we have our two finalists
-	movie_info[1] = [pairing1[0], pairing2[0]]
-	return FINAL(movie_info)
+def vote(obj, num_participants):
+	if len(obj) > 2:
+		films = {}
+		for person in obj:
+			print("These are the two films for " + person + ": ", obj[person])
+			vote1 = int(input("How many people vote for " + obj[person][0] + "? "))
+			# second film gets (n - vote1) votes
+			vote2 = (num_participants - vote1)
+			if vote1 > vote2:
+				films[person] = obj[person][0]
+				print("\n")
+			elif vote2 > vote1:
+				films[person] = obj[person][1]
+				print("\n")
+			else:
+				print("Coin toss!")
+				print("...")
+				time.sleep(5)
+				result = random.choice(obj[person])
+				print(result + " passes!")
+				films[person] = result
+				print("\n")
+		return films
+	elif len(obj) == 2:
+		print("The two movies are:", obj)
+		vote1 = int(input("How many people vote for " + obj[0] + "? "))
+		# second film gets (n - vote1) votes
+		vote2 = (num_participants - vote1)
+		if vote1 > vote2:
+			return obj[0]
+		elif vote2 > vote1:
+			return obj[1]
+		else:
+			print("Coin toss!")
+			time.sleep(5)
+			result = random.choice(obj)
+			print(result + " passes")
+			return result
 
 
 def RANKING(movie_info):
@@ -51,16 +50,17 @@ def RANKING(movie_info):
 	# key = movie, val = points?
 	movies = {}
 	for person in n_films:
-		# every movie will start at 0
+		# every movie will start at 0 (i.e., TDK: 0)
 		movies[n_films[person]] = 0
-		print(n_films.values())
-		print("These are the films available.  Please rank your top 4 (or 3) from best to worst!")
+		print([n_films[movie] for movie in n_films])
+		print(person + ", these are the films available.  Please rank your top 4 (or 3) from best to worst!")
 		movie_ranks = {}
 		for i in range(1, len(n_films) + 1):
 			movie = input("#" + str(i) + "? ")
 			movie_ranks[i] = movie
 		# each person has their own dictionary of movies and their ranks
 		people[person] = movie_ranks
+		print("\n")
 	# tally up the ranks
 	for person in people:
 		for rank in people[person]:
@@ -73,63 +73,59 @@ def RANKING(movie_info):
 		# remove film with highest number of points
 		movies.popitem()
 		movie_info[1] = [movie for movie in movies]
-		return FINAL(movie_info)
+		return movie_info
 	else:
 		movie_info[1] = [movie for movie in movies]
-		return PAIRINGS(movie_info)
+		return movie_info
 
 
-# this function takes in a dictionary with names as keys and movie lists as values
-# returns the dictionary with half the number of films!
-def ROUND1(movie_info):
-	# key = person, val = 1 film
-	n_films = {}
-	movie_dictionary = movie_info[0]["Movie Choices"]
-	# iterate thru dictionary with 2n films
-	# should finish with n films
-	for person in movie_dictionary:
-		print("These are your two films: ", movie_dictionary[person])
-		vote1 = int(input("How many people vote for " + movie_dictionary[person][0] + "? "))
-		# second film gets (n - vote1) votes
-		vote2 = (len(movie_dictionary) - vote1)
-		if vote1 > vote2:
-			n_films[person] = movie_dictionary[person][0]
-		elif vote2 > vote1:
-			n_films[person] = movie_dictionary[person][1]
-		else:
-			print("Coin toss!")
-			result = random.choice(movie_dictionary[person])
-			print(result + " passes")
-			n_films[person] = result
-	# at this point we eliminated n films
-	movie_info.append(n_films)
-	return RANKING(movie_info)
-
-
-def movie_choices():
+def main():
 	day = date.today()
 	theme = input("What's the movie theme for tonight? ")
 	people = []
 	num_people = int(input("How many people are there in the table? "))
-
 	for person in range(num_people):
 		name = input("Input a name: ")
 		people.append(name)
-
+	print("\n")
 	# people as keys and movie lists as values
 	movie_dict = dict()
-
 	for name in people:
 		movie1 = input("1st movie for " + name + "? ")
 		movie2 = input("2nd movie for " + name + "? ")
 		# each person has two movies
 		movie_dict[name] = [movie1, movie2]
+		print("\n")
 	info = [{"Date": day, "Theme": theme, "People": people, "Movie Choices": movie_dict}]
-	return ROUND1(info)
-
-
-def main():
-	movie_choices()
+	print("\n")
+	# ROUND 1
+	# key = person, val = 1 film
+	# should finish with n films
+	n_films = vote(movie_dict, len(info["People"]))
+	info.append(n_films)
+	print("\n")
+	# RANKING
+	info = RANKING(info)
+	# PAIRING/FINAL
+	if len(info[1]) > 2:
+		movies = info[1]
+		# 1 v 4
+		pairing1 = [movies[0], movies[3]]
+		# 2 v 3
+		pairing2 = [movies[1], movies[2]]
+		winner1 = vote(pairing1, len(info["People"]))
+		winner2 = vote(pairing2, len(info["People"]))
+		print("FINAL!")
+		winner = vote([winner1, winner2], len(info["People"]))
+		info.append(winner)
+		print("And the winner is ... " + winner + "!")
+		return info
+	else:
+		print("FINAL!")
+		winner = vote(info[1], len(info["People"]))
+		info.append(winner)
+		print("And the winner is ... " + winner + "!")
+		return info
 
 
 if __name__ == '__main__':
